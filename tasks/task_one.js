b2d66aaf147f43bfaa4338a1dc50af845071d3b2
Chidubem <nwekedubemph@gmail.com>
2023-09-07 04:59:07 -0700

    first commit

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -1,2 +1,30 @@
+const form = document.getElementById('note-form');
+const notes = document.getElementById('notes');
+const noteInput = document.getElementById('note');
 
-// Write something
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newNote = { text: noteInput.value };
+    saveNote(newNote);
+    renderNotes();
+    noteInput.value = '';
+});
+
+function saveNote(note) {
+    const notesArray = getSavedNotes();
+    notesArray.push(note);
+    localStorage.setItem('notes', JSON.stringify(notesArray));
+}
+
+function getSavedNotes() {
+    const notesStr = localStorage.getItem('notes');
+    return JSON.parse(notesStr) || [];
+}
+
+function renderNotes() {
+    const notesArray = getSavedNotes();
+    const noteElements = notesArray.map((note) => `<div class="note">${note.text}</div>`);
+    notes.innerHTML = noteElements.join('\n');
+}
+
+renderNotes();
